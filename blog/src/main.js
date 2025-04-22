1a50269ed6695afb4d94730eb4a13bf44042e41d
983033995 <983033995@qq.com>
2025-04-22 10:53:46 +0800

    refactor: Update main.js for Vue 3 and Pinia

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,15 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+// 可以在这里引入全局样式文件
+// import './assets/main.css'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+const app = createApp(App)
+
+app.use(createPinia()) // 启用 Pinia
+app.use(router)
+
+app.mount('#app')
